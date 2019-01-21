c773fb7720363d3ca40214cb28f97ae7d1810d74
Rosana <rodjuga@gmail.com>
2019-01-21 23:50:20 +0100

    @Html helpers for .NET

---
+++ HtmlHelpers/Controllers/HtmlHelpersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HtmlHelpers.Controllers
+{
+    public class HtmlHelpersController : Controller
+    {
+        // GET: HtmlHelpers
+        public ViewResult OsnovniHelperi()
+        {
+            return View();
+        }
+    }
+}
